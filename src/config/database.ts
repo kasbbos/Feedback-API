5213305a7ffa225867ef56962458fa28ee12c7de
kasbbos <kasbbos@gmail.com>
2024-12-25 10:38:09 +0500

    database.ts update

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,18 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || '',
-  process.env.DB_USER || '',
-  process.env.DB_PASS || '',
-  {
-    host: process.env.DB_HOST || '',
-    dialect: 'postgres',
-    logging: false,
-  }
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASS as string,
+    {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT || 5432),
+        dialect: 'postgres',
+        logging: false,
+    }
 );
 
 export default sequelize;
